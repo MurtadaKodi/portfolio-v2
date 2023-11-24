b2bdd9babaf78acd5bb2f608905bcb5fb7a78e33
‪murtada kodi‬‏ <murtadakodi69@gmail.com>
2023-11-24 18:59:22 +0300

    town-v2

--- src/pages/5-page4-img.jsx
+++ src/pages/5-page4-img.jsx
@@ -3,88 +3,107 @@ import CardMedia from "@mui/material/CardMedia";
 
 export default function Page4ImageCard() {
   return (
-    <section className="card-container">
-      <Card className="card-layout" >
-        <CardMedia
-          className="img-card"
-          component="img"
-          alt="green iguana"
-          height="100%"
+    
+    
+  <div>
+      <section className="card-container">
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+          
+    
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069412/WebSite%20Images/Alzubara%20image/20231102_115952_ldu7lk.jpg"
+          />
         
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069399/WebSite%20Images/Alzubara%20image/20231102_115931_euocqj.jpg"
+          />
+          
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1686040908/WebSite%20Images/Sites/Alzubarah-2_ne8ndo.jpg"
+          />
+        
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            // height="400"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069836/WebSite%20Images/Alzubara%20image/20231025_084802_a78ifl.jpg"
+          />
+          
+        </Card>
+      </section>
 
-          image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069412/WebSite%20Images/Alzubara%20image/20231102_115952_ldu7lk.jpg"
-        />
-        {/* <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-      
-          <Typography className='site-details'>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent> */}
-      </Card>
+      <div className='divider'/>
 
-      <Card className="card-layout" >
-        <CardMedia
-          className="img-card"
-          component="img"
-          alt="green iguana"
-          height="100%"
-          image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069399/WebSite%20Images/Alzubara%20image/20231102_115931_euocqj.jpg"
-        />
-        {/* <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-      
-          <Typography className='site-details' variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent> */}
-      </Card>
+      <section className="card-container">
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+          
+    
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069412/WebSite%20Images/Alzubara%20image/20231102_115952_ldu7lk.jpg"
+          />
+        
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069399/WebSite%20Images/Alzubara%20image/20231102_115931_euocqj.jpg"
+          />
+          
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            height="100%"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1686040908/WebSite%20Images/Sites/Alzubarah-2_ne8ndo.jpg"
+          />
+        
+        </Card>
+    
+        <Card className="card-layout" >
+          <CardMedia
+            className="img-card"
+            component="img"
+            alt="green iguana"
+            // height="400"
+            image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069836/WebSite%20Images/Alzubara%20image/20231025_084802_a78ifl.jpg"
+          />
+          
+        </Card>
+      </section>
+  </div>
 
-      <Card className="card-layout" >
-        <CardMedia
-          className="img-card"
-          component="img"
-          alt="green iguana"
-          height="100%"
-          image="https://res.cloudinary.com/dmklduciw/image/upload/v1686040908/WebSite%20Images/Sites/Alzubarah-2_ne8ndo.jpg"
-        />
-        {/* <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-      
-          <Typography className='site-details' variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent> */}
-      </Card>
-
-      <Card className="card-layout" >
-        <CardMedia
-          className="img-card"
-          component="img"
-          alt="green iguana"
-          // height="400"
-          image="https://res.cloudinary.com/dmklduciw/image/upload/v1700069836/WebSite%20Images/Alzubara%20image/20231025_084802_a78ifl.jpg"
-        />
-        {/* <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-      
-          <Typography className='site-details' variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent> */}
-      </Card>
-    </section>
   );
 }
