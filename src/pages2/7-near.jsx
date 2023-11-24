49783bb493a288042409fdd09fc7b33a80005235
‪murtada kodi‬‏ <murtadakodi69@gmail.com>
2023-11-24 13:28:37 +0300

    site

--- src/pages2/7-near.jsx
+++ src/pages2/7-near.jsx
@@ -12,29 +12,29 @@ const Near = () => {
       <div>
         <section className="grid-container ">
           <div className='grid1'><img className='grid1-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319691/WebSite%20Images/Alzubara%20image/17680_uv4hmy.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1689241357/WebSite%20Images/Sites/grid_large_preview_hpmvhe.jpg" alt="" /></div>
           <div className='grid2'><img className='grid2-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17691_mmhlp4.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1689241091/WebSite%20Images/Sites/grid_landscape_efl784.jpg" alt="" /></div>
           <div className='grid3'> <img className='grid3-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/25660_e4ee9b.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1686040922/WebSite%20Images/Sites/Al_Mafjar-3_cujxmz.jpg" alt="" /></div>
           <div className='grid4'> <img className='grid4-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17713_xuitcy.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1686040915/WebSite%20Images/Sites/Al_Mafjar-1_bvrdbf.jpg" alt="" /></div>
           <div className='grid5'> <img className='grid5-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17709_cychyz.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1686040918/WebSite%20Images/Sites/Lejmail-5_dgazav.jpg" alt="" /></div>
         </section>
         
 
         <section className="grid-container ">
           <div className='grid1'><img className='grid1-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319691/WebSite%20Images/Alzubara%20image/17680_uv4hmy.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1689241627/WebSite%20Images/Sites/grid_landscape_k3g0mi.jpg" alt="" /></div>
           <div className='grid2'><img className='grid2-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17691_mmhlp4.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1689241336/WebSite%20Images/Sites/grid_landscape_nqvxkr.jpg" alt="" /></div>
           <div className='grid3'> <img className='grid3-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/25660_e4ee9b.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1689241357/WebSite%20Images/Sites/grid_large_preview_hpmvhe.jpg" alt="" /></div>
           <div className='grid4'> <img className='grid4-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17713_xuitcy.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1686040922/WebSite%20Images/Sites/Al_Mafjar-5_bfmmxc.jpg" alt="" /></div>
           <div className='grid5'> <img className='grid5-img' 
-          src="https://res.cloudinary.com/dmklduciw/image/upload/v1691319692/WebSite%20Images/Alzubara%20image/17709_cychyz.jpg" alt="" /></div>
+          src="https://res.cloudinary.com/dmklduciw/image/upload/v1686040905/WebSite%20Images/Sites/AinMhd-1_ijee12.jpg" alt="" /></div>
         </section>
       </div>
       <section className="details-map-container ">
